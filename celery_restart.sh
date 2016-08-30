df501db7e729ea2bb496d004bffa398f5585e4dc
timercrack <timercrack@gmail.com>
2016-08-30 17:40:16 +0800

    update celery

--- celery_restart.sh
+++ celery_restart.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 kill `cat celeryworker.pid`
+kill `cat celerybeat.pid`
 sleep 2
 rm -f celery*.log
-celery worker --pidfile=celeryworker.pid --logfile=celeryworker.log -A dashboard -l INFO --detach -B
+celery worker --pidfile=celeryworker.pid --logfile=celeryworker.log -A dashboard -l INFO --detach
+celery beat --pidfile=celerybeat.pid --logfile=celerybeat.log -A dashboard -l INFO --detach
 echo 'done!'
