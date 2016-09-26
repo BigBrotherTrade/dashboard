244e9cdb676e2be16d29dac74373072e9c5e94b3
timercrack <timercrack@gmail.com>
2016-09-26 22:28:55 +0800

    调整滑动条样式

--- static/js/bar.js
+++ static/js/bar.js
@@ -3,13 +3,18 @@ var myChart = echarts.init(document.getElementById('main'));
 
 option = {
     backgroundColor: '#000000',
-    color: ['#ff0','#0f0', '#61a0a8', '#d48265', '#91c7ae','#749f83',  '#ca8622', '#bda29a','#6e7074', '#546570', '#c4ccd3'],
+    color: ['#ff0', '#0f0', '#61a0a8', '#d48265', '#91c7ae', '#749f83', '#ca8622', '#bda29a', '#6e7074', '#546570', '#c4ccd3'],
     title: {
         text: '交易信号',
         left: 0,
         textStyle: {
             color: '#ffdf00'
         }
+    },
+    animation: false,
+    grid: {
+        left: '4%',
+        right: '3%'
     },
     tooltip: {
         trigger: 'axis',
@@ -40,10 +45,11 @@ option = {
                 color: '#9c0000'
             }
         },
-        splitLine: {show: false}
+        boundaryGap: false
     },
     yAxis: {
         type: 'value',
+        splitNumber: 3,
         splitLine: {
             lineStyle: {
                 color: '#9c0000'
@@ -54,13 +60,24 @@ option = {
                 color: '#9c0000'
             }
         },
-        scale: true
+        scale: true,
+        min: 'dataMin',
+        max: 'dataMax'
     },
     dataZoom: [
         {
             show: true,
             type: 'slider',
-            start: 0,
+            backgroundColor: '#000',
+            fillerColor: '#212421',
+            borderColor: '#525152',
+            handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+            handleSize: '70%',
+            textStyle: {
+                color: '#94aede'
+            },
+            throttle: 0,
+            start: 80,
             end: 100
         }
     ],
